4482f4fc04260f649310fa7cd4e280f154d58e43
Andreas Karpasitis <karpasitis@outlook.com>
2019-04-19 03:49:51 +0200

    chore: update main app content and css

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 import style from './App.scss';
 
-const App = () => <h1 className={style.header}>React App</h1>;
+const App = () => (
+  <div className={style.wrapper}>
+    <h1 className={style.header}>React Boilerplate</h1>
+    <p>React / Redux Boilerplate with Webpack. ESLint setup and SCSS Support</p>
+  </div>
+);
 
 export default App;
